c1e36b86e4782ce377273f9133783df4084c1a26
Ishmael87 <ishmael.direro@korridor.com>
2024-08-24 17:38:55 +0200

    Created PageObjects for KorridorLogin Page

---
+++ src/test/java/PageObjects/KorridorLoginFunctionality/KorridorUserCredentialsTest.java
@@ -0,0 +1,51 @@
+package PageObjects.KorridorLoginFunctionality;
+
+import net.serenitybdd.annotations.Step;
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+class KorridorUserCredentialsTest extends PageObject {
+
+    // Korridor Front End Url
+
+    String Url = "https://apptest.korridor.com/login";
+
+    // Korridor Login Page Elements
+
+    String UserNameXpath = "//input[@id='username']";
+    String PasswordXpath = "//input[@id='password']";
+    String SignInButton = "//button[@id='login-btn']";
+
+
+    // Korridor Login Page Methods
+
+    @Step("Enter UserName")
+    public void EnterUserName(String UserName){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(UserNameXpath))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(UserNameXpath))).sendKeys(UserName);
+    }
+
+    @Step("Enter Password")
+    public void EnterPassword(String Password){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PasswordXpath))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PasswordXpath))).sendKeys(Password);
+    }
+
+    @Step("Click LogIn Button")
+    public void ClickLogInButton(){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SignInButton))).click();
+
+    }
+
+
+
+}
